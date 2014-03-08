7fbf7cec82f45d3578a2e2bc2e95aefb8c21887a
Cristian Pereyra <cristian@redmintlabs.com>
2014-03-08 13:04:29 -0300

    add activesupport

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ gemspec
 
 group :test do
   gem 'activerecord'
+  gem 'activesupport'
   gem 'hobo_fields'
   gem 'rspec'
 end
---
+++ spec/fields_spec.rb
@@ -0,0 +1,8 @@
+require 'fields'
+require 'active_record'
+
+describe Fields do
+  it "should be a module" do
+    Fields.should_not be_nil 
+  end
+end
